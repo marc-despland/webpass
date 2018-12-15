8a896a7001028540ecf5b8129eb6174d95add9b9
Marc DESPLAND <marc.despland@orange.com>
2018-12-15 13:06:49 +0100

    implementing JOIN protocol

--- api/index.js
+++ api/index.js
@@ -42,10 +42,15 @@ wss.on('connection', function connection(ws) {
 	ws.on('message', function incoming(message) {
 		var action=JSON.parse(message);
 		if (action.hasOwnProperty('code')) {
+			console.log("Received Message "+ action.code);
 			switch (action.code) {
 				case "LISTEN" :
 					//generate a channel id 
 					var channel=Math.floor((Math.random() * 10000000) + 1);
+					while (channels.hasOwnProperty(channel)) {
+						channel=Math.floor((Math.random() * 10000000) + 1);
+					}
+					console.log("	Creating Channel "+channel);
 					channels[channel]={
 						listen: ws
 					};
@@ -56,7 +61,9 @@ wss.on('connection', function connection(ws) {
 					ws.send(JSON.stringify(response));
 					break;
 				case "CONNECT" :
-					if (action.hasOwnProperty('channel')) {
+					//Send by the connect PEER to connect to the listen one
+					if (action.hasOwnProperty('channel') && channels.hasOwnProperty(action.channel)) {
+						console.log("	Channel "+action.channel);
 						var channel=channels[action.channel];
 						if ((channel!==undefined) && action.hasOwnProperty('payload')) {
 							channels[action.channel].connected=ws;
@@ -73,54 +80,111 @@ wss.on('connection', function connection(ws) {
 						}
 					}
 					break;
-				case "CONNECT_FAILED" :
-					if (action.hasOwnProperty('channel')) {
+				case "JOIN_REQUEST" :
+					//Send by the connect PEER to connect to the listen one
+					if (action.hasOwnProperty('channel') && channels.hasOwnProperty(action.channel)) {
+						console.log("	Channel "+action.channel);
 						var channel=channels[action.channel];
-						if (channel!==undefined) {
+						if ((channel!==undefined) && action.hasOwnProperty('key')) {
+							channels[action.channel].connected=ws;
 							var response={};
-							response.code="CONNECT_FAILED";
+							response.code="JOIN_REQUEST";
 							response.channel=action.channel;
-							channels[action.channel].connected.send(JSON.stringify(response));
-							channels[action.channel].connected.close();
-							channels[action.channel].connected=undefined;
+							response.key=action.key;
+							channels.listen.send(JSON.stringify(response));
+						}
+					}
+					break;
+				case "JOIN_CHALLENGE" :
+					//Send by the connect PEER to connect to the listen one
+					if (action.hasOwnProperty('channel') && channels.hasOwnProperty(action.channel)) {
+						console.log("	Channel "+action.channel);
+						var channel=channels[action.channel];
+						if ((channel!==undefined) && action.hasOwnProperty('payload')) {
+							if (channel.listen===ws) {
+								var response={};
+								response.code="JOIN_CHALLENGE";
+								response.channel=action.channel;
+								response.payload=action.payload;
+								channel.connected.send(JSON.stringify(response));
+							}
+						}
+					}
+					break;
+				case "JOIN_SUCCESS" :
+					//Send by the connect PEER to connect to the listen one
+					if (action.hasOwnProperty('channel') && channels.hasOwnProperty(action.channel)) {
+						console.log("	Channel "+action.channel);
+						var channel=channels[action.channel];
+						if ((channel!==undefined) && action.hasOwnProperty('payload')) {
+							if (channel.connected===ws) {
+								var response={};
+								response.code="JOIN_SUCCESS";
+								response.channel=action.channel;
+								response.payload=action.payload;
+								channel.listen.send(JSON.stringify(response));
+							}
+						}
+					}
+					break;
+				case "CONNECT_FAILED" :
+					//Send by the listen PEER to indicate it reject the connection
+					//Listen peer should close the connection
+					if (action.hasOwnProperty('channel') && channels.hasOwnProperty(action.channel)) {
+						console.log("	Channel "+action.channel);
+						var channel=channels[action.channel];
+						if (channel.listen===ws) {
+							if (channel!==undefined) {
+								var response={};
+								response.code="CONNECT_FAILED";
+								response.channel=action.channel;
+								channel.connected.send(JSON.stringify(response));
+								channel.connected.close();
+								channel.listen.close();
+								delete channels[action.channel];
+							}
 						} else {
-							var response={};
-							response.code="FAILED";
-							response.message="No valid channel provided";
-							ws.send(JSON.stringify(response));
+							console.log("	Invalid Peer ... this is not the listen one");
 						}
 					}
 					break;
 				case "CONNECTED" :
-					if (action.hasOwnProperty('channel')) {
+					if (action.hasOwnProperty('channel') && channels.hasOwnProperty(action.channel)) {
 						var channel=channels[action.channel];
-						if ((channel!==undefined) && action.hasOwnProperty('payload')) {
-							var response={};
-							response.code="CONNECTED";
-							response.channel=action.channel;
-							response.payload=action.payload;
-							channels[action.channel].connected.send(JSON.stringify(response));
+						console.log("	Channel "+action.channel);
+						if (channel.listen===ws) {
+							if ((channel!==undefined) && action.hasOwnProperty('payload')) {
+								var response={};
+								response.code="CONNECTED";
+								response.channel=action.channel;
+								response.payload=action.payload;
+								channel.connected.send(JSON.stringify(response));
+							} else {
+								var response={};
+								response.code="FAILED";
+								response.message="No valid channel provided";
+								ws.send(JSON.stringify(response));
+							}
 						} else {
-							var response={};
-							response.code="FAILED";
-							response.message="No valid channel provided";
-							ws.send(JSON.stringify(response));
+							console.log("	Invalid Peer ... this is not the connected one");
 						}
-						
 					}
 					break;
 				case "MESSAGE" :
-					if (action.hasOwnProperty('channel')) {
+					if (action.hasOwnProperty('channel') && channels.hasOwnProperty(action.channel)) {
 						var channel=channels[action.channel];
+						console.log("	Channel "+action.channel);
 						if ((channel!==undefined) && action.hasOwnProperty('payload')) {
 							var response={};
 							response.code="MESSAGE";
 							response.channel=action.channel;
 							response.payload=action.payload;
-							if (ws===channels[action.channel].connected) {
+							if (ws===channel.connected) {
 								channels[action.channel].listen.send(JSON.stringify(response));
-							} else {
+							} else if (ws===channel.listen) {
 								channels[action.channel].connected.send(JSON.stringify(response));
+							} else {
+								console.log("	Invalid Peer ... ");
 							}
 						} else {
 							var response={};
@@ -132,29 +196,58 @@ wss.on('connection', function connection(ws) {
 					}
 					break;
 				case "CONNECTED_FAILED" :
-					if (action.hasOwnProperty('channel')) {
+					if (action.hasOwnProperty('channel') && channels.hasOwnProperty(action.channel)) {
 						var channel=channels[action.channel];
-						if (channel!==undefined) {
-							var response={};
-							response.code="CONNECTED_FAILED";
-							response.channel=action.channel;
-							channels[action.channel].listen.send(JSON.stringify(response));
-							ws.close();
-							channels[action.channel].connected=undefined;
+						console.log("	Channel "+action.channel);
+						if (channel.connected===ws) {
+							if (channel!==undefined) {
+								var response={};
+								response.code="CONNECTED_FAILED";
+								response.channel=action.channel;
+								channel.listen.send(JSON.stringify(response));
+								channel.connected.close();
+								channel.listen.close();
+								delete channels[action.channel]
+							} else {
+								var response={};
+								response.code="FAILED";
+								response.message="No valid channel provided";
+								ws.send(JSON.stringify(response));
+							}
 						} else {
-							var response={};
-							response.code="FAILED";
-							response.message="No valid channel provided";
-							ws.send(JSON.stringify(response));
+							console.log("	Invalid Peer ... this is not the connected one");
 						}
 					}
 					break;
 			}
 		}
-		console.log('received: %s', message);
 	});
 	ws.on('close', function closing(message) {
 		console.log(`closed: ${message}`);
+		for(var key in channels) {
+			if (channels.hasOwnProperty(key)) {
+				var channel=channels[key];
+				if (channel!==undefined) {
+					if (channel.listen===ws) {
+						console.log("	Channel "+key);
+						var response={};
+						response.code="CLOSE";
+						response.message="Remote Peer close the connection";
+						channel.connectied.send(JSON.stringify(response));
+						channel.connected.close();
+						delete channels[key];
+					} else if (channel.connected===ws) {
+						console.log("	Channel "+key);
+						var response={};
+						response.code="CLOSE";
+						response.message="Remote Peer close the connection";
+						channel.listen.send(JSON.stringify(response));
+						channel.listen.close();
+						delete channels[key];
+					}
+				}
+			}
+		}
 	});
 	ws.on('error', function onerror(message) {
 		console.log(`onerror: ${message}`);
