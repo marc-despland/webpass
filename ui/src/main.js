ec5d5eafb0686d14a1550d81a266276689586e26
Marc DESPLAND <marc.despland@orange.com>
2018-12-15 18:27:13 +0100

    Adding challenge management to all message

--- ui/src/main.js
+++ ui/src/main.js
@@ -7,8 +7,8 @@ import VueNativeSock from 'vue-native-websocket'
 import VueClipboards from 'vue-clipboards';
 
 
-//const url = "wss://webpass.kmt.orange.com/api/socket"
-const url= "ws://172.17.0.2:8080/api/socket"
+const url = "wss://webpass.kmt.orange.com/api/socket"
+//const url= "ws://172.17.0.2:8080/api/socket"
 //var dataws = new WebSocket(url) , WebSocket: dataws
 Vue.use(VueNativeSock, url, { store: store, connectManually: true})
 Vue.use(VueClipboards);
